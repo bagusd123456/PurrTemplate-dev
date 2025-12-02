842c59e5211d3ca78bfbce96a270800f03a00dc7
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2025-12-02 18:47:39 +0700

    Fix bug

--- Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
+++ Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
@@ -75,8 +75,6 @@ public class PrometeoCarController : PredictedIdentity<CarInputData, CarInputHan
 
     protected override void LateAwake()
     {
-        if (!isServer) return;
-
         ulong clientId = 0;
         string ownerName = "NULL";
         if (owner.HasValue)
@@ -136,7 +134,7 @@ public class PrometeoCarController : PredictedIdentity<CarInputData, CarInputHan
                 gameObject.AddComponent<AudioListener>();
             }
         }
-        else
+        else if (isServer)
         {
             voiceInterface.Init(clientId, ownerName);
 
